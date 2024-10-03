715f31d71c8018f4a18cbe63eebae6dd843b941a
nicolasoyarce <nicolasOyarce@users.noreply.github.com>
2024-10-03 18:42:30 -0300

    Initial Commit

---
+++ app/(routes)/(dashboard)/layout.tsx
@@ -0,0 +1,3 @@
+export default function LayoutDashboard({children}: {children: React.ReactNode}) {
+    return <div>{children}</div>
+}
---
+++ app/(routes)/page.tsx
@@ -0,0 +1,9 @@
+export default function Home() {
+  return (
+    <div>
+      <div className="h-full flex items-center justify-center">
+        From Page home
+      </div>
+    </div>
+  );
+}
