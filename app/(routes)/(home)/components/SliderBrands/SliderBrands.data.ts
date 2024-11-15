59e900ad655d017007fec6088fab3a19ce272b14
nicolasoyarce <nicolasOyarce@users.noreply.github.com>
2024-11-15 04:38:24 -0300

    add sliderbrand and firtsblock components to the home

---
+++ app/(routes)/(home)/components/SliderBrands/SliderBrands.data.ts
@@ -0,0 +1,32 @@
+export const dataBrands = [
+    {
+        url: "karate.png",
+    },
+    {
+        url: "boxeo.png",
+    },
+    {
+        url: "boxeo2.png",
+    },
+    {
+        url: "boxeo4.webp",
+    },
+    {
+        url: "karate2.png",
+    },
+    {
+        url: "academy.png",
+    },
+    {
+        url: "academy2.png",
+    },
+    {
+        url: "futbol.png",
+    },
+    {
+        url: "cueca.png",
+    },
+    {
+        url: "danza.webp",
+    },
+];
