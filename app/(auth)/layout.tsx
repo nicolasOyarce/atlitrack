e3118a58c4be7b64442bbabe75b45ba01053d098
nicolasoyarce <nicolasOyarce@users.noreply.github.com>
2024-11-30 04:57:29 -0300

    design sign-in

--- app/(auth)/layout.tsx
+++ app/(auth)/layout.tsx
@@ -1,19 +1,17 @@
-import React from 'react'
-import Image from 'next/image'
-import { Navbar } from '@/components/Navbar'
+import React from 'react';
+import Image from 'next/image';
 
-export default function AuthLayout({children}: {children: React.ReactNode}) {
+export default function AuthLayout({ children }: { children: React.ReactNode }) {
     return (
-        
-            <div className=''>
-                <div className='bg-black text-white'><Navbar /></div>    
-                <div className='grid lg:grid-cols-2 '>     
-                    <div className=' bg-slate-200 items-center justify-center bg-transparent'>{children}</div>
-                    <div className='hidden lg:flex lg:bg-slate-200 h-full justify-center items-center lg:flex-col'>
-                    <Image src="/image-sign.png" alt="AtliTrack" width={830} height={600} />
+        <div className='bg-gray-950'>
+            <div className='grid lg:grid-cols-2'>
+                <div className='bg-slate-200 items-center justify-center bg-transparent'>
+                    {children}
+                </div>
+                <div className='hidden lg:flex h-full justify-center items-center lg:flex-col bg-green-500 relative rounded-tl-full'>
+                    <Image src="/images/boxing.png" alt="Overlay Image" width={549} height={650} />
                 </div>
             </div>
-            </div>
-        
-    )
+        </div>
+    );
 }
--- app/(auth)/sign-in/[[...sign-in]]/page.tsx
+++ app/(auth)/sign-in/[[...sign-in]]/page.tsx
@@ -2,9 +2,9 @@
 
 import { useAuth } from "@/app/api/auth/login/route"
 import { useState } from "react"
-import { Button } from "@/components/ui/button"
-import { Input } from "@/components/ui/input"
-import { Label } from "@/components/ui/label"
+import Image from "next/image"
+import Link from 'next/link';
+
 
 export default function LoginPage() {
   const { login, isLoading } = useAuth()
@@ -24,50 +24,82 @@ export default function LoginPage() {
     try {
       await login(formData.username, formData.password)
     } catch (error) {
-      // El error ya es manejado en el contexto
       console.error(error)
       setErrorMessage("Error de autenticación. Verifica tus credenciales.");
     }
   }
 
   return (
-    <div className="flex items-center justify-center min-h-[90vh]">
-      <form onSubmit={handleSubmit} className="flex-1 space-y-4 w-full max-w-md p-8">
-        <div>
-          <Label htmlFor="" className="text-4xl text-slate-500 font-bold pl-2">Ingresa aquí!</Label>
+    <div className="min-h-screen bg-gray-950 text-gray-100 flex items-center justify-center">
+      <div className="w-full max-w-screen-md bg-gray-950 shadow sm:rounded-lg p-6 sm:p-12">
+        <div className="flex flex-col items-center">
+          <Image src="logo.svg" alt="Company Logo" className="mb-8" width={290} height={290} />
+          <h1 className="text-5xl font-bold text-white mb-2">Iniciar Sesión</h1>
+          <h3 className="text-lg text-gray-400">Bienvenido a AtliTrack</h3>
+
+          
+          <div className="my-12 border-b text-center">
+              <div
+                className="leading-none px-2 inline-block text-sm text-gray-400 tracking-wide font-medium bg-gray-950 transform translate-y-1/2">
+                Ingresa tus credenciales
+              </div>
+            </div>
+
+          <form onSubmit={handleSubmit} className="w-full max-w-md space-y-6">
+            <div>
+              <input
+                id="email"
+                type="email"
+                placeholder="Correo"
+                value={formData.username}
+                onChange={(e) =>
+                  setFormData((prev) => ({ ...prev, username: e.target.value }))
+                }
+                disabled={isLoading}
+                className="w-full px-8 py-4 rounded-lg font-medium bg-gray-700 border border-gray-600 placeholder-gray-400 text-sm focus:outline-none focus:border-gray-500 focus:bg-gray-600"
+              />
+            </div>
+
+            <div>
+              <input
+                id="password"
+                type="password"
+                placeholder="Contraseña"
+                value={formData.password}
+                onChange={(e) =>
+                  setFormData((prev) => ({ ...prev, password: e.target.value }))
+                }
+                disabled={isLoading}
+                className="w-full px-8 py-4 rounded-lg font-medium bg-gray-700 border border-gray-600 placeholder-gray-400 text-sm focus:outline-none focus:border-gray-500 focus:bg-gray-600"
+              />
+            </div>
+            <a href="#" className="text-sm text-blue-400 hover:underline mt-2 block text-center">
+              ¿Olvidaste tu contraseña?
+            </a>
+
+            <button
+              type="submit"
+              className="w-full py-4 rounded-lg font-semibold bg-green-500 text-white hover:bg-green-600 transition-all duration-300 ease-in-out focus:outline-none focus:shadow-outline"
+              disabled={isLoading}
+            >
+              {isLoading ? "Iniciando sesión..." : "Iniciar sesión"}
+            </button>
+          </form>
+
+          
+          <div className="mt-6 text-center">
+            <Link href="/" className="text-sm text-blue-400 hover:underline">
+              Volver al inicio
+            </Link>
+            <p className="mt-4 text-xs text-gray-400">
+              <Link href="#" className="border-b border-gray-500 border-dotted">
+                Política de privacidad
+              </Link>
+            </p>
+          </div>
         </div>
-        <div className="mt-6">
-          <Label className="text-xl p-2 text-slate-500">Correo eléctronico</Label>
-          <Input
-          type="email"
-          placeholder="Email"
-          value={formData.username}
-          onChange={(e) => 
-            setFormData(prev => ({ ...prev, username: e.target.value }))
-          }
-          disabled={isLoading}
-        />
-        </div>
-        <div className="mt-2">
-          <Label className="p-2 text-xl text-slate-500">Contraseña</Label>
-          <Input
-            type="password"
-            placeholder="Contraseña"
-            value={formData.password}
-            onChange={(e) => 
-              setFormData(prev => ({ ...prev, password: e.target.value }))
-            }
-            disabled={isLoading}
-          /> 
-        </div>
-        
-        <div className="">
-          <Button type="submit" className="w-full text-lg p-8 font-semibold" disabled={isLoading}>
-          {isLoading ? "Iniciando sesión..." : "Iniciar sesión"}
-        </Button>
-        </div>
-        
-      </form>
+      </div>
     </div>
+
   )
 }
